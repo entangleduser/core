965b27519a940e329b875338b890653dd48c2691
William Luke <williamluke@me.com>
2023-04-04 23:16:38 -0500

    add `Builder` protocol

---
+++ Sources/Composite/Composite.swift
@@ -0,0 +1,43 @@
+/// https://github.com/apple/swift-evolution/blob/main/proposals/0289-result-builders.md
+protocol Builder {
+ associatedtype Expression
+ typealias FinalResult = [Expression]
+}
+
+enum Either<T,U> {
+ case first(T)
+ case second(U)
+}
+
+indirect enum ResultBuilderTerm<Expression> {
+ case expression(Expression)
+ case block([ResultBuilderTerm])
+ case either(Either<ResultBuilderTerm, ResultBuilderTerm>)
+ case optional(ResultBuilderTerm?)
+}
+
+@resultBuilder
+protocol VariadicBuilder {
+ associatedtype Expression
+ typealias Component = ResultBuilderTerm<Expression>
+ typealias FinalResult = [Expression]
+ static func buildFinalResult(_ component: Component) -> FinalResult
+}
+
+extension VariadicBuilder {
+ static func buildExpression(_ expression: Expression) -> Component {
+  .expression(expression)
+ }
+ static func buildBlock(_ components: Component...) -> Component {
+  .block(components)
+ }
+ static func buildOptional(_ component: Component?) -> Component {
+  .optional(component)
+ }
+ static func buildArray(_ components: [Component]) -> Component {
+  .block(components)
+ }
+ static func buildLimitedAvailability(_ component: Component) -> Component {
+  component
+ }
+}
